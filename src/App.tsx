0dfaeb10cb67301a4f48c8d1769ae8e60681ed1e
Hasan Çelikoğlu <myspotifyacc@yandex.com>
2023-03-15 15:30:09 +0300

    feat: react-router-dom added and home page route written

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import { MantineProvider, Text } from '@mantine/core';
+import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import { Login } from './pages/Login';
 
 export default function App() {
   return (
     <MantineProvider withGlobalStyles withNormalizeCSS>
-      <Home/>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path="*" element={<h1>404 - Page not found!</h1>} />
+      </Routes>
     </MantineProvider>
   );
 }
