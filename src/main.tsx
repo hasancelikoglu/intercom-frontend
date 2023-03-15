7c4731ef3ee0755e8f49af75cc1926cbac82e58f
Hasan Çelikoğlu <myspotifyacc@yandex.com>
2023-03-15 16:29:19 +0300

    feat: global css file added

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
+import './styles/global.css'
 
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-    <App />
+      <App />
     </BrowserRouter>
-      
   </React.StrictMode>,
 )
