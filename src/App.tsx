62c6d684e9f77179d75c789e9d6c2af4c89085b0
recepkefelii <recepkefeliii@gmail.com>
2023-03-15 15:53:13 +0300

    fix: register router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import { MantineProvider, Text } from '@mantine/core';
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
-<<<<<<< HEAD
 import { Register } from './pages/Register';
-=======
+
 import { Login } from './pages/Login';
->>>>>>> 0cfff5918b8613a334d7d76acdf44bc3af3d357a
+
 
 export default function App() {
   return (
@@ -13,10 +12,9 @@ export default function App() {
       <Routes>
         <Route path='/' element={<Home/>} />
         <Route path="*" element={<h1>404 - Page not found!</h1>} />
-<<<<<<< HEAD
+
         <Route path="/" element={<Register/>} />
-=======
->>>>>>> 0cfff5918b8613a334d7d76acdf44bc3af3d357a
+
       </Routes>
     </MantineProvider>
   );
