c3e22baf2f99f3160c2301b6afb9cdd937a624b4
recepkefelii <recepkefeliii@gmail.com>
2023-03-15 15:33:26 +0300

    chore: component call

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { MantineProvider, Text } from '@mantine/core';
 import { Login } from './pages/Login';
+import { Register } from './pages/Register';
 
 export default function App() {
   return (
     <MantineProvider withGlobalStyles withNormalizeCSS>
       <Text>Welcome to Mantine!</Text>
       <Login />
+      <Register/>
     </MantineProvider>
   );
 }
