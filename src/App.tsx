f29bd83f2218398ccfb7f5021c1fd46cb5da3100
Hasan Çelikoğlu <myspotifyacc@yandex.com>
2023-03-18 19:46:55 +0300

    feat: Login page written

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Topics from './components/Topics/Topics';
 import Home from './pages/Home';
 import { EditProfile } from './components/Profile/EditProfile';
 import { User } from './components/Profile/User';
+import { Login } from './pages/Login';
 
 export default function App() {
   return (
@@ -24,6 +25,8 @@ export default function App() {
           <Route path='profile/settings' element={<EditProfile/>} />
           <Route path='topics' element={<Topics/>} />
         </Route>
+
+        <Route path='/auth/login' element={<Login/>} />
       </Routes>
     </MantineProvider>
   );
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,45 +1,70 @@
 import {
+    Paper,
+    createStyles,
     TextInput,
     PasswordInput,
     Checkbox,
-    Anchor,
-    Paper,
+    Button,
     Title,
     Text,
-    Container,
-    Group,
-    Button,
-  } from '@mantine/core';
-  
-  export function Login() {
+    Anchor,
+    rem,
+} from '@mantine/core';
+import { IconArrowBackUp } from '@tabler/icons-react';
+
+import { useNavigate } from 'react-router-dom';
+
+const useStyles = createStyles((theme) => ({
+    wrapper: {
+        minHeight: rem(900),
+        backgroundSize: 'cover',
+        backgroundImage:
+            'url(https://images.unsplash.com/photo-1484242857719-4b9144542727?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1280&q=80)',
+    },
+
+    form: {
+        borderRight: `${rem(1)} solid ${theme.colorScheme === 'dark' ? theme.colors.dark[7] : theme.colors.gray[3]
+            }`,
+        minHeight: rem(900),
+        maxWidth: rem(450),
+        paddingTop: rem(80),
+
+        [theme.fn.smallerThan('sm')]: {
+            maxWidth: '100%',
+        },
+    },
+
+    title: {
+        color: theme.colorScheme === 'dark' ? theme.white : theme.black,
+        fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+    },
+}));
+
+export function Login() {
+    const { classes } = useStyles();
+    const navigate = useNavigate()
     return (
-      <Container size={420} my={40}>
-        <Title
-          align="center"
-          sx={(theme) => ({ fontFamily: `Greycliff CF, ${theme.fontFamily}`, fontWeight: 900 })}
-        >
-          Welcome back!
-        </Title>
-        <Text color="dimmed" size="sm" align="center" mt={5}>
-          Do not have an account yet?{' '}
-          <Anchor size="sm" component="button">
-            Create account
-          </Anchor>
-        </Text>
-  
-        <Paper withBorder shadow="md" p={30} mt={30} radius="md">
-          <TextInput label="Email" placeholder="you@mantine.dev" required />
-          <PasswordInput label="Password" placeholder="Your password" required mt="md" />
-          <Group position="apart" mt="lg">
-            <Checkbox label="Remember me" />
-            <Anchor component="button" size="sm">
-              Forgot password?
-            </Anchor>
-          </Group>
-          <Button fullWidth mt="xl">
-            Sign in
-          </Button>
-        </Paper>
-      </Container>
+        <div className={classes.wrapper}>
+            <Paper className={classes.form} radius={0} p={30}>
+                <IconArrowBackUp color='white' onClick={() => navigate(-1)} />
+                <Title order={2} className={classes.title} ta="center" mt="md" mb={50}>
+                    Welcome back to Intercom!
+                </Title>
+
+                <TextInput label="Email address" placeholder="hello@gmail.com" size="md" />
+                <PasswordInput label="Password" placeholder="Your password" mt="md" size="md" />
+                <Checkbox label="Keep me logged in" mt="xl" size="md" />
+                <Button fullWidth mt="xl" size="md">
+                    Login
+                </Button>
+
+                <Text ta="center" mt="md">
+                    Don&apos;t have an account?{' '}
+                    <Anchor<'a'> href="#" weight={700} onClick={(event) => event.preventDefault()}>
+                        Register
+                    </Anchor>
+                </Text>
+            </Paper>
+        </div>
     );
-  }
+}
