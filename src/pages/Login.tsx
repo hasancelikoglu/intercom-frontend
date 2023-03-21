d8c0eac72bc840759d2247141514dbc2dfc57ad1
Hasan Çelikoğlu <myspotifyacc@yandex.com>
2023-03-21 21:53:22 +0300

    "fix: min height problem solved"

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -23,7 +23,7 @@ import { setToken, setUser } from '../utils/auth';
 
 const useStyles = createStyles((theme) => ({
     wrapper: {
-        minHeight: rem(900),
+        height: "100vh",
         backgroundSize: 'cover',
         backgroundImage:
             'url(https://images.unsplash.com/photo-1484242857719-4b9144542727?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1280&q=80)',
@@ -32,7 +32,7 @@ const useStyles = createStyles((theme) => ({
     form: {
         borderRight: `${rem(1)} solid ${theme.colorScheme === 'dark' ? theme.colors.dark[7] : theme.colors.gray[3]
             }`,
-        minHeight: rem(900),
+        height: "100%",
         maxWidth: rem(450),
         paddingTop: rem(80),
 
